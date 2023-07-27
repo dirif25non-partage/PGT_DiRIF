0c02fb5e2d0e1847e7037af748e529655feb7b0d
Olivier Nalin <olivier.nalin@developpement-durable.gouv.fr>
2023-07-27 18:26:42 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,15 +1,7 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'myproject'
-copyright = '2023, on'
+project = 'ForteCongestion'
+copyright = '2043, on'
 author = 'on'
-release = '0.1'
+release = ''
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
